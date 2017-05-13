781ba2d12ba3c0658ed59792e953265ccb9bbc37
sennevb <sennevanbaelen@gmail.com>
2017-05-13 08:53:07 +0200

    Update rpcmisc.cpp

--- src/rpcmisc.cpp
+++ src/rpcmisc.cpp
@@ -734,6 +734,7 @@ int start = 0;
         delta.push_back(Pair("satoshis", it->second));
         delta.push_back(Pair("txid", it->first.txhash.GetHex()));
         delta.push_back(Pair("index", (int)it->first.index));
+        delta.push_back(Pair("blockindex", (int)it->first.txindex));
         delta.push_back(Pair("height", it->first.blockHeight));
         delta.push_back(Pair("address", address));
         result.push_back(delta);
